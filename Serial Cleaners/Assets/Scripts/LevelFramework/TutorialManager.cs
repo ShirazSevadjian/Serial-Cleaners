0a11cfeaf95e8327cffecec804c4cd9d2e02d4c9
danny roux <danny.roux@hotmail.fr>
2022-11-10 01:07:11 -0500

    Added tutorial manager

---
+++ Serial Cleaners/Assets/Scripts/LevelFramework/TutorialManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    public GameObject player1;
+    public GameObject player2;
+
+    private int popUpIndex;
+    bool bloodCleaned;
+
+    bool didPressE;
+
+    void Start()
+    {
+        popUpIndex = 0;
+        bloodCleaned = false;
+        didPressE = false;
+        UpdateTutorial();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (popUpIndex == 0)
+        {
+            if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Horizontal2") || Input.GetButtonDown("Vertical") || Input.GetButtonDown("Vertical2"))
+            {
+                popUpIndex++;
+                UpdateTutorial();
+            }
+        }
+        else if (popUpIndex == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.E) || didPressE)
+            {
+                didPressE = !didPressE;
+                if (player1.transform.Find("InteractablePosition/Bodybag") || player2.transform.Find("InteractablePosition/Bodybag"))
+                {
+                    popUpIndex++;
+                    UpdateTutorial();
+                }
+            }
+        }
+        else if (popUpIndex == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                popUpIndex++;
+                UpdateTutorial();
+            }
+        }
+        else if (popUpIndex == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.E) || didPressE)
+            {
+                didPressE = !didPressE;
+                if (player1.transform.Find("InteractablePosition/Mop") || player2.transform.Find("InteractablePosition/Mop"))
+                {
+                    popUpIndex++;
+                    UpdateTutorial();
+                }
+            }
+        }
+        else if (popUpIndex == 4)
+        {
+            if (bloodCleaned)
+            {
+                popUpIndex++;
+                UpdateTutorial();
+            }
+        }
+    }
+
+    void UpdateTutorial()
+    {
+        for(int i = 0; i < popUps.Length; ++i)
+        {
+            popUps[i].SetActive(i == popUpIndex);
+        }
+    }
+
+    public void onBloodCleaned()
+    {
+        bloodCleaned = true;
+    }
+}
