dc794442cc862f8bef63a12913ee3b9e5dc8f89f
danny roux <danny.roux@hotmail.fr>
2022-11-13 17:30:05 -0500

    Controller player interactions

--- Serial Cleaners/Assets/Scripts/Characters/Player1Input.cs
+++ Serial Cleaners/Assets/Scripts/Characters/Player1Input.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class Player1Input : MonoBehaviour
 {
@@ -10,36 +11,102 @@ public class Player1Input : MonoBehaviour
 
     // Animation.
     [SerializeField] private Animator myAnimator;
+
     private bool isMoving;
+    private bool isForward;
+    private bool isDiagonal;
+    private bool isSideways;
+    private bool isBackwardDiagonal;
+    private bool isBackward;
+    private bool mirror;
+
+    PlayerControls controls;
+    Vector2 move;
+    Vector2 lastMove;
+    Vector2 look;
 
     private void Start()
     {
         // Find components.
         myAnimator = GetComponentInChildren<Animator>();
+        look = new Vector2(0, -1);
+        lastMove = new Vector2(0, -1);
     }
 
     private void Update()
     {
         // Obtain input information (See "Horizontal" and "Vertical" in the Input Manager)
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        //float horizontal = Input.GetAxisRaw("Horizontal");
+        //float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+        //Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+        //direction = direction.normalized;
+
+        //// Translate the gameobject
+        //transform.position += direction * speed * Time.deltaTime;
+
+        //// Rotate the gameobject
+        //if (direction != Vector3.zero)
+        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction, Vector3.up), rotationSpeed * Time.deltaTime);
+
+        Vector3 direction = new Vector3(move.x, 0.0f, move.y);
         direction = direction.normalized;
 
-        // Translate the gameobject
         transform.position += direction * speed * Time.deltaTime;
 
-        // Rotate the gameobject
-        if (direction != Vector3.zero)
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction, Vector3.up), rotationSpeed * Time.deltaTime);
+        if(look != Vector2.zero)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(new Vector3(look.x, 0, look.y), Vector3.up), rotationSpeed * Time.deltaTime);
 
-
-        // Update animation variables.
-        isMoving = !Mathf.Approximately(horizontal, 0f) || !Mathf.Approximately(vertical, 0f);
+        //// Update animation variables.
+        //isMoving = !Mathf.Approximately(move.x, 0f) || !Mathf.Approximately(move.y, 0f);
 
         // Update animator.
-        myAnimator.SetBool("isMoving", isMoving);
+        UpdateAnimator();
+    }
 
+    void UpdateAnimator()
+    {
+        float dot = Vector2.Dot(lastMove, look);
+        isForward = dot >= 0.90;
+        isDiagonal = 0.25 <= dot && dot < 0.90;
+        isSideways = -0.25 <= dot && dot < 0.25;
+        isBackwardDiagonal = -0.75 <= dot && dot < -0.25;
+        isBackward = dot < -0.75;
+        mirror = lastMove.x * look.y - lastMove.y * look.x < 0;
+
+        myAnimator.SetBool("IsForward", isForward);
+        myAnimator.SetBool("IsMoving", isMoving);
+        myAnimator.SetBool("IsDiagonal", isDiagonal);
+        myAnimator.SetBool("IsSideways", isSideways);
+        myAnimator.SetBool("IsBackwardDiagonal", isBackwardDiagonal);
+        myAnimator.SetBool("IsBackward", isBackward);
+        myAnimator.SetBool("Mirror", mirror);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            move = -context.ReadValue<Vector2>();
+            if(move != Vector2.zero)
+            {
+                lastMove = move.normalized;
+                isMoving = true;
+            }
+        }
+        else if (context.canceled)
+        {
+            move = Vector2.zero;
+            isMoving = false;
+        }
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            look = -context.ReadValue<Vector2>();
+            look = look.normalized;
+        }
     }
 }
--- Serial Cleaners/Assets/Scripts/LevelFramework/TutorialManager.cs
+++ Serial Cleaners/Assets/Scripts/LevelFramework/TutorialManager.cs
@@ -13,6 +13,8 @@ public class TutorialManager : MonoBehaviour
 
     bool didPressE;
 
+    private bool playerMove = false;
+    private bool playerSubmit = false;
     private bool bodyPickedUp = false;
     private bool bodyDestroyed = false;
     private bool mopPickedUp = false;
@@ -29,70 +31,16 @@ public class TutorialManager : MonoBehaviour
         UpdateTutorial();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (popUpIndex == 0)
-        {
-            if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Horizontal2") || Input.GetButtonDown("Vertical") || Input.GetButtonDown("Vertical2"))
-            {
-                popUpIndex++;
-                UpdateTutorial();
-            }
-        }
-        else if (popUpIndex == 1)
-        {
-            if (Input.GetKeyDown(KeyCode.E) || didPressE)
-            {
-                didPressE = !didPressE;
-                if (player1.transform.Find("InteractablePosition/Bodybag") || player2.transform.Find("InteractablePosition/Bodybag"))
-                {
-                    popUpIndex++;
-                    UpdateTutorial();
-                }
-            }
-        }
-        else if (popUpIndex == 2)
-        {
-            if (Input.GetKeyDown(KeyCode.X))
-            {
-                popUpIndex++;
-                UpdateTutorial();
-            }
-        }
-        else if (popUpIndex == 3)
-        {
-            if (Input.GetKeyDown(KeyCode.E) || didPressE)
-            {
-                didPressE = !didPressE;
-                if (player1.transform.Find("InteractablePosition/Mop") || player2.transform.Find("InteractablePosition/Mop"))
-                {
-                    popUpIndex++;
-                    UpdateTutorial();
-                }
-            }
-        }
-        else if (popUpIndex == 4)
-        {
-            if (bloodCleaned)
-            {
-                popUpIndex++;
-                UpdateTutorial();
-            }
-        }
-    }
-
-
     private IEnumerator TutorialSequence()
     {
         Time.timeScale = 0;
-        yield return new WaitUntil(() => Input.GetButtonDown("Submit"));
+        yield return new WaitUntil(() => playerSubmit);
 
         Time.timeScale = 1;
         popUpIndex++;
         UpdateTutorial();
 
-        yield return new WaitUntil(() => Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Horizontal2") || Input.GetButtonDown("Vertical") || Input.GetButtonDown("Vertical2"));
+        yield return new WaitUntil(() => playerMove);
 
         popUpIndex++;
         UpdateTutorial();
@@ -128,6 +76,16 @@ public class TutorialManager : MonoBehaviour
 
         popUpIndex++;
         UpdateTutorial();
+    }
+
+    public void PlayerSubmit()
+    {
+        playerSubmit = true;
+    }
+
+    public void PlayerMove()
+    {
+        playerMove = true;
     }
 
     public void InteractedWithBody()
